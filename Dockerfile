5b6d77d56bb028752d72025b5981151435cb892f
sandeeprz <sandeep_cverma@yahoo.com>
2019-07-27 14:24:21 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM centos:7.3.1611 as builder
+
+ADD https://rpm.nodesource.com/setup_8.x /root/
+#RUN curl -sL https://rpm.nodesource.com/setup_8.x
+RUN bash /root/setup_8.x ;\  
+yum -y install nodejs ;\
+yum -y install java-1.8.0-openjdk ;\
+yum install vim -y
+
+
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+
+
+RUN npm run build
+
+FROM nginx
+
+COPY --from=builder /app/build /usr/share/nginx/html
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to AirTel DT World</h2>
+        </div>
+        <p className="App-intro">
+        <a href="wwww.dtopspod.com">Hi Team how are you</a>
+        
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
